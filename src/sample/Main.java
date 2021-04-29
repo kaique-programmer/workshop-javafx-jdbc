71daa30a1d514017160c007043c025e25fabf691
kaique oliveira santos <kaiquevicio@gmail.com>
2021-04-29 18:09:42 -0300

    About view

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -10,6 +10,7 @@ import javafx.stage.Stage;
 import java.io.IOException;
 
 public class Main extends Application {
+    private static Scene mainScene;
 
     /*@Override
     public void start(Stage primaryStage) throws Exception{
@@ -28,13 +29,17 @@ public class Main extends Application {
             scrollPane.setFitToHeight(true);
             scrollPane.setFitToWidth(true);
 
-            Scene mainScene = new Scene(scrollPane);
+            mainScene = new Scene(scrollPane);
             primaryStage.setScene(mainScene);
             primaryStage.setTitle("Sample JavaFX application");
             primaryStage.show();
         } catch (IOException e) {
             e.printStackTrace();
         }
+    }
+
+    public static Scene getMainScene() {
+        return mainScene;
     }
 
     public static void main(String[] args) {
