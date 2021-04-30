1912ef9e36d8df3f512553669e733bc0a3e14790
kaique oliveira santos <kaiquevicio@gmail.com>
2021-04-29 21:54:33 -0300

    DepartmentList controller

---
+++ src/gui/DepartmentListController.java
@@ -0,0 +1,46 @@
+package gui;
+
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.stage.Stage;
+import model.entities.Department;
+import sample.Main;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class DepartmentListController implements Initializable {
+    @FXML
+    private TableView<Department> departmentTableView;
+
+    @FXML
+    private TableColumn<Department, Integer> tableColumnId;
+
+    @FXML
+    private TableColumn<Department, String> tableColumnName;
+
+    @FXML
+    private Button buttonNew;
+
+    @FXML
+    public void onButtonNewAction() {
+        System.out.println("onButtonNewAction");
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        initializeNodes();
+    }
+
+    private void initializeNodes() {
+        tableColumnId.setCellValueFactory(new PropertyValueFactory<>("id"));
+        tableColumnName.setCellValueFactory(new PropertyValueFactory<>("name"));
+
+        Stage stage = (Stage) Main.getMainScene().getWindow();
+        departmentTableView.prefHeightProperty().bind(stage.heightProperty());
+    }
+}
--- src/gui/MainViewController.java
+++ src/gui/MainViewController.java
@@ -43,7 +43,7 @@ public class MainViewController implements Initializable {
 
 
     @Override
-    public void initialize(URL uri, ResourceBundle resourceBundle) {
+    public void initialize(URL url, ResourceBundle resourceBundle) {
 
     }
 
